6b52f41702e937b0d2fa8e9e9624af110614a527
coryesurv <104436291+coryesurv@users.noreply.github.com>
2024-10-23 21:38:15 +0100

    update

---
+++ api/ehouse_register.py
@@ -0,0 +1,86 @@
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+EHOUSE_API_BASE_URL = os.getenv('EHOUSE_API_BASE_URL')
+EHOUSE_USERNAME = os.getenv('EHOUSE_USERNAME')
+EHOUSE_PASSWORD = os.getenv('EHOUSE_PASSWORD')
+EHOUSE_BRANCH_ID = os.getenv('EHOUSE_BRANCH_ID')
+EHOUSE_BRANCH_SECRET = os.getenv('EHOUSE_BRANCH_SECRET')
+EHOUSE_ACCOUNT_ACTIVATION_TOKEN = os.getenv('EHOUSE_ACCOUNT_ACTIVATION_TOKEN')
+
+def register_ehouse_api_account():
+    """
+    Here you will supply us with API user account information alongside with the branchId 
+    and the branchSecret you recieved from our staff. If succesfull you will recieve an email 
+    with a token to activate your account.
+    """
+    
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    }
+
+    data = {
+        "title": "string",
+        "firstName": "string",
+        "lastName": "string",
+        "jobTitle": "string",
+        "email": "string",
+        "userName": EHOUSE_USERNAME,
+        "password": EHOUSE_PASSWORD,
+        "confirmPassword": EHOUSE_PASSWORD,
+        "branchId": EHOUSE_BRANCH_ID,
+        "branchSecret": EHOUSE_BRANCH_SECRET
+    }
+
+    url = EHOUSE_API_BASE_URL + 'Account/Register'
+
+    response = requests.post(url=url, headers=headers, json=data)
+    
+    return response
+
+def activate_ehouse_api_account():
+    """
+    This where you actiavte your APi account with the token you recieved from previous step.
+    """
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    }
+
+    data = {
+        "userName": EHOUSE_USERNAME,
+        "accountActivationToken": EHOUSE_ACCOUNT_ACTIVATION_TOKEN
+    }
+    
+    url = EHOUSE_API_BASE_URL + 'Account/Activate'
+
+    response = requests.post(url=url, headers=headers, json=data)
+    
+    return response
+
+def get_ehouse_token():
+    
+    headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Accept': 'application/json',
+    }
+    
+    data = {
+        'grant_type': 'password',
+        'username': EHOUSE_USERNAME,
+        'password': EHOUSE_PASSWORD,
+    }
+
+    response = requests.post('https://api.ehouse.co.uk/token', headers=headers, data=data, verify='d:\python\lib\site-packages\certifi\cacert.pem')
+    return response
+
+import certifi
+
+print(certifi.where())
+
+token_response = get_ehouse_token()
+print(token_response)
