0b343ed777e10bfd87f4408f984a9ade3b9dd527
coryesurv <104436291+coryesurv@users.noreply.github.com>
2024-10-31 09:47:15 +0000

    update

--- api/api.py
+++ api/api.py
@@ -1,9 +1,9 @@
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify, send_from_directory, redirect, url_for
 from flask_cors import CORS
-from api.book_ehouse_job import book_ehouse_job
-from api.book_surveyhub_job import book_surveyhub_job
-from api.validate_token import validate_token
+from book_ehouse_job import book_ehouse_job
+from book_surveyhub_job import book_surveyhub_job
+from validate_token import validate_token
 import os
 from functools import wraps
 import esurv_db_manager as es
@@ -117,6 +117,8 @@ def submit_form():
         phone_number = data['telephone']
         address = data['selectedAddress']
         region = address['region']
+        
+        
 
         if region == 'Scotland':
             book_surveyhub_job(address['building_name_number'], address['street'], address['postcode'], first_name, last_name, email_address, phone_number)
