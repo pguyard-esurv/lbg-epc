b93c7eae1d32dc288610ced1642dcc6dc8d2fc44
coryesurv <104436291+coryesurv@users.noreply.github.com>
2024-11-01 15:39:59 +0000

    update

--- api/validate_token.py
+++ api/validate_token.py
@@ -21,9 +21,6 @@ def validate_token(token):
     
     correlation_id = str(uuid.uuid4())
     session_id = str(uuid.uuid4())
-    
-    correlation_id = "2846ceb2-afa7-4422-99d0-e260fb079b81"
-    session_id = "2712678b-f155-40e9-ad78-3e191fea3529"
 
     headers = {
         'x-correlation-id': correlation_id,
@@ -34,8 +31,8 @@ def validate_token(token):
         'Accept-Encoding': 'gzip, deflate, br',
         'Connection': 'keep-alive',
         'User-Agent':'e.surv-user',
-        #'origin': 'https://mortgages.secure.halifax-online.co.uk/',
-        #'Referer': 'https://mortgages.secure.halifax-online.co.uk/home/external-apis/sustainability/v1/epc/token'
+        'origin': 'https://lbg-epc.esurv.co.uk/',
+        'Referer': 'https://lbg-epc.esurv.co.uk/'
     }
 
     data = {
@@ -43,22 +40,14 @@ def validate_token(token):
     }
     
     if PROD_STATUS == 'dev':
-        print('fail')
         return 'valid'
 
     try:
         response = requests.put(url, headers=headers, json=data, verify=False)
-        
-        print(response)
-        print(response.text)
-        
-        #verify=False)
-        
-        #return response
 
         if response.status_code == 200:
             json_response = response.json()
-            status = json_response.get('payload', {}).get('status')
+            status = json_response.get('status')
             return status  # Expected values: 'valid', 'invalid', 'expired', 'used'
         else:
             json_response = response.json()
@@ -69,6 +58,7 @@ def validate_token(token):
         return f"Exception occurred: {str(e)}"
 
 """
+
 PROD_STATUS = 'prod'
 
 token = '43edc231106d834b894f364087232c5a:7e2d040228b35f18b7c043786ac73b9c344a92d0c92bd9a5d5457cf694ff6bb4069e9cc540424cf9e42ed52a628f69cb6d40e52b5f237871c73dec4a3c13f924073c275d218ff3a9af67805310f25376c2c29db8e9f6f901d535f6158b59c06ac05d322bd7b14bd04a44c99c522433fe'
@@ -79,10 +69,10 @@ token = 'valid'
 
 token = '7c2f4e002d10b82d583ddc82:9350aefef679d5b2e7fde4d75c7abfc7798e4282c10d479445042b1cbc00e4dac7733510953b1e40b0259de10a8d8e3be94163a059abb36d564bb9007ce1e3d174b99e41a23525dc8795052bd2834160c99331248405bf7769a6d28df886e75061c3:a57532340bc1ce453d1b81bbd30e4686'
 
-                                                    
+token = 'valid'                                          
 
-response = validate_token(token)
+validity = validate_token(token)
 
-print(response)
-print(response.text)
+print(validity)
+
 """
