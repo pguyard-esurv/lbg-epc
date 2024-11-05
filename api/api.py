9befbb1dc15b59a5037205450d3632ad85171b17
coryesurv <104436291+coryesurv@users.noreply.github.com>
2024-11-05 22:32:23 +0000

    update

--- api/api.py
+++ api/api.py
@@ -113,7 +113,7 @@ def index():
 @app.route('/')
 def index():
     import psycopg2
-    #cnx = psycopg2.connect(user="psqladmin", password=os.getenv('DB_PASSWORD'), host="10.180.10.132", port=5432, database="postgres")
+    cnx = psycopg2.connect(user="psqladmin", password=os.getenv('DB_PASSWORD'), host="10.180.10.132", port=5432, database="postgres")
     return "success"
 
 """
