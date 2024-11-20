a2806fa0307e7841930c17053b59068d7038efc7
coryesurv <104436291+coryesurv@users.noreply.github.com>
2024-11-20 11:48:16 +0000

    update

--- api/book_ehouse_job.py
+++ api/book_ehouse_job.py
@@ -2,14 +2,14 @@ import json
 import os
 import requests
 from dotenv import load_dotenv
+import sentry_sdk
 
 load_dotenv()
 EHOUSE_USERNAME = os.getenv('EHOUSE_USERNAME')
 EHOUSE_PASSWORD = os.getenv('EHOUSE_PASSWORD')
 EHOUSE_API_BASE_URL = os.getenv('EHOUSE_API_BASE_URL')
-EHOUSE_ACCESS_TOKEN = os.getenv('EHOUSE_ACCESS_TOKEN')
 EHOUSE_KEY_INVOICE_ITEM_ID = os.getenv('EHOUSE_KEY_INVOICE_ITEM_ID')
-EHOUSE_CLIENT_INVOICE_ITEM_CODE = os.getenv('EHOUSE_CLIENT_INVOICE_ITEM_CODE')
+EHOUSE_CLIENT_INVOICE_ITEM_CODE = int(os.getenv('EHOUSE_CLIENT_INVOICE_ITEM_CODE'))
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 pem_file_path = os.path.join(base_dir, 'custom_ca_bundle.pem')
@@ -66,16 +66,16 @@ def send_book_ehouse_job_request(street_address, postcode, town, name, email_add
                 "quantity": 1
             }
         ],
-        "buildingNumber": "string",
-        "buildingName": "string",
-        "subBuilding": "string",
+        #"buildingNumber": "string",
+        #"buildingName": "string",
+        #"subBuilding": "string",
         "streetAddress": street_address, #required
         "town": town, #required
         "postcode": postcode, #required
         "sendVendorBookingSms": True, #required
         "sendVendorBookingEmail": True, #required
         "vendorName": name, #required
-        "vendorTelephone": phone_number,
+        "VendorMobile": phone_number,
         "vendorEmail": email_address, #required
     }
     
@@ -87,26 +87,25 @@ def send_book_ehouse_job_request(street_address, postcode, town, name, email_add
 
 def book_ehouse_job(street_address, postcode, town, name, email_address, phone_number):
     
+    ehouse_access_token = get_ehouse_token()
+    
     try:
         key_invoice_item_id = get_key_invoice_item_id()
-    except:
+    except Exception as e:
+        sentry_sdk.capture_exception(e)
         key_invoice_item_id = EHOUSE_KEY_INVOICE_ITEM_ID
-    
-    ehouse_access_token = EHOUSE_ACCESS_TOKEN
     
     response = send_book_ehouse_job_request(street_address, postcode, town, name, email_address, phone_number, key_invoice_item_id, ehouse_access_token)
     
-    if response.status_code == 401:
-        ehouse_access_token = get_ehouse_token()
-        response = send_book_ehouse_job_request(street_address, postcode, town, name, email_address, phone_number, key_invoice_item_id, ehouse_access_token)
-
-        if response.status_code != 200:
-            print('ehouse api request successfully sent')
-            raise Exception(response.json())
-        #poss: send sentry email saying that the token is bad
+    if response.status_code != 200:
+        print('ehouse api request successfully sent')
+        raise Exception(response.json())
     
     if response.status_code != 200:
             raise Exception(response.json())
+
+
+EHOUSE_ACCESS_TOKEN = ''
 
 def get_ehouse_orders():
     headers = {
@@ -135,7 +134,6 @@ def delete_ehouse_order(order_id):
     return response
 
 #'rowVersion': 2038904407, 'orderNumber': 8620495,
-    
 
 def delete_all_ehouse_orders():
     response = get_ehouse_orders()
